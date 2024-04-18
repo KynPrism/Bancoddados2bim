c7d0a6d22dafd21e1e6a6e81a0260f4f0c41b0ac
Pedro Henrique Sampaio <134095568+KynPrism@users.noreply.github.com>
2024-04-17 22:11:46 -0300

    Add files via upload

---
+++ OWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW.sql
@@ -0,0 +1,23 @@
+/*
+
+1-  select * from PRODUTOS;
+
+2-  select NOME, PREÇO 
+    from PRODUTOS
+    where PREÇO > 100;
+
+3-  select NOME, QTD_ESTOQUE
+    from PRODUTOS
+    where QTD_ESTOQUE < 20;
+
+4-  select NOME, PREÇO
+    from PRODUTOS
+    where PREÇO < 50 or PREÇO between 50 and 100 or PREÇO > 100;
+
+5-  select NOME, PREÇO * 1.1
+    from PRODUTOS;
+
+6-  select NOME, PREÇO, QTD_ESTOQUE
+    from PRODUTOS
+    order by QTD_ESTOQUE desc;
+*/
